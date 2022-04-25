1d5523b2e2cd09eaa5b24b78b04daa43e2038e18
Popo21 <39035077+Fierda@users.noreply.github.com>
2022-04-25 10:19:08 +0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -598,7 +598,7 @@ folium.Choropleth(
 ).add_to(m)
 folium.features.GeoJson(indonesia, name="LSOA Code",
                         popup=folium.features.GeoJsonPopup(fields=['state'])).add_to(m)
-folium_static(m,width=1400, height=500)
+folium_static(m,width=1380, height=500)
 
 
 df_kons = pd.read_excel(filekons)
